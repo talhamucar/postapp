a3d4eb567204c33b310a45256dbb9ebb4c5b5473
mtucar <talhamucar@gmail.com>
2021-01-30 15:03:51 -0500

    Frontend template is finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PostCreateComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule
